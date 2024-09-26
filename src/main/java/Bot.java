ddd1c807ff11e95e94dc4b3f58f6b2d0708d23ca
PavelMartinelli <pav.shipitsyn@gmail.com>
2024-09-26 18:13:03 +0500

    Add commands + Add don't functional inline button

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,12 +1,17 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.AnswerCallbackQuery;
 import org.telegram.telegrambots.meta.api.methods.CopyMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
 import org.telegram.telegrambots.meta.api.objects.InputFile;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+import java.util.List;
 
 public class Bot extends TelegramLongPollingBot {
 
@@ -17,7 +22,8 @@ public class Bot extends TelegramLongPollingBot {
         Bot bot = new Bot();
         botsApi.registerBot(bot);
         bot.sendText(798230948L, "Доброе утро");
-        bot.sendPhoto(798230948L, new InputFile("https://i.pinimg.com/originals/a1/57/e3/a157e33152dbf0d9da5627b28a070bd6.jpg"));
+        bot.sendPhoto(798230948L,
+                new InputFile("https://i.pinimg.com/originals/a1/57/e3/a157e33152dbf0d9da5627b28a070bd6.jpg"));
     }
 
     @Override
@@ -26,11 +32,66 @@ public class Bot extends TelegramLongPollingBot {
         var user = message.getFrom();
         var id = user.getId();
 
-        System.out.println("Имя: " + user.getFirstName() + " Фамилия: " + user.getLastName() + " Написал сообщение: " + message.getText());
-        System.out.println("ID: " + user.getId() + message.hasPhoto());
+        System.out.println(update.hasCallbackQuery());
 
+        if(update.hasCallbackQuery()) {
+            String call_data = update.getCallbackQuery().getData();
+            System.out.println(update.getCallbackQuery().getData());
 
-        copyMessage(id, message.getMessageId());
+            if (call_data.equals("food")) {
+                sendText(id, "Тут будет еда");
+            }
+        }
+
+        if(message.isCommand()){
+            switch (message.getText()){
+                case "/find" :
+                    sendMenu(id);
+                    break;
+
+                case "/add" :
+                    sendText(id, "Тут будет добавление рецептов");
+                    break;
+
+                case "/login" :
+                    sendText(id, "Тут будет вход");
+                    break;
+
+                case "/register" :
+                    sendText(id, "Тут будет регистрация");
+                    break;
+                default: break;
+            }
+        }
+        else
+            copyMessage(id, message.getMessageId());
+
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    public void sendMenu(Long User_id){
+        String txt = "<b>Рицепт чего хотите найти?</b>";
+
+        InlineKeyboardButton back = InlineKeyboardButton.builder()
+                .text("Назад").callbackData("back")
+                .build();
+        InlineKeyboardButton drinks = InlineKeyboardButton.builder()
+                .text("Напитки").callbackData("drink").build();
+        InlineKeyboardButton food = InlineKeyboardButton.builder()
+                .text("Еда").callbackData("food").build();
+        InlineKeyboardMarkup menu_keyboard = InlineKeyboardMarkup.builder()
+                .keyboardRow(List.of(drinks, food))
+                .keyboardRow(List.of(back))
+                .build();
+
+        SendMessage menu = SendMessage.builder().chatId(User_id.toString())
+                .parseMode("HTML").text(txt)
+                .replyMarkup(menu_keyboard).build();
+        try {
+            execute(menu);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void copyMessage(Long who, Integer msgId){
