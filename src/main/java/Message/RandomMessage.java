c5d387117ff021f9e597367c0a03bb966cc2e8a3
Антон <novikov.anton.serg@gmail.com>
2024-10-25 11:42:51 +0500

    Новые сообщения

---
+++ src/main/java/Message/RandomMessage.java
@@ -0,0 +1,49 @@
+package Message;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomMessage {
+    String RandomText = "Это рандом да..... Пока в разработке.";
+
+    public SendMessage createMessage(long chatId) {
+        return SendMessage.builder()
+                .chatId(chatId)
+                .text(RandomText)
+                .replyMarkup(createKeyboard())
+                .build();
+    }
+
+    public EditMessageText createEditMessage(long chatId, int messageId) {
+        return EditMessageText.builder()
+                .chatId(String.valueOf(chatId))
+                .messageId(messageId)
+                .text(RandomText)
+                .replyMarkup(createKeyboard())
+                .build();
+    }
+
+    private InlineKeyboardMarkup createKeyboard() {
+        List<InlineKeyboardRow> rows = new ArrayList<>();
+
+        rows.add(createRow("На главное меню", "/back"));
+
+        return InlineKeyboardMarkup.builder().keyboard(rows).build();
+    }
+
+    private InlineKeyboardRow createRow(String buttonText, String callbackData) {
+        InlineKeyboardButton button = InlineKeyboardButton.builder()
+                .text(buttonText)
+                .callbackData(callbackData)
+                .build();
+        InlineKeyboardRow row = new InlineKeyboardRow();
+        row.add(button);
+        return row;
+    }
+}
