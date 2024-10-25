9ee1624545aaf07570194f1bafca883f62d6f1ae
Антон <novikov.anton.serg@gmail.com>
2024-10-25 07:51:24 +0500

    Ну тут много писать что я сделал... Перетянул свою логигу сюда.

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,69 +1,142 @@
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.TelegramBotsApi;
-import org.telegram.telegrambots.meta.api.methods.CopyMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
-import org.telegram.telegrambots.meta.api.objects.CallbackQuery;
-import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
 
-import java.util.ArrayList;
-import java.util.List;
+import static java.lang.Math.toIntExact;
 
-public class Bot extends TelegramLongPollingBot {
-    private InlineKeyboardMarkup menu;
-    Tg_Token token = new Tg_Token();
+public class Bot implements LongPollingSingleThreadUpdateConsumer {
+    private final TelegramClient telegramClient;
+    private final MenuMessage MenuContent;
+    private final HelpMessage helpContent;
 
-    public static void main(String[] args) throws TelegramApiException {
-        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-        Bot bot = new Bot();
-        botsApi.registerBot(bot);
+    public Bot(String botToken) {
+        telegramClient = new OkHttpTelegramClient(botToken);
+        MenuContent = new MenuMessage();
+        helpContent = new HelpMessage();
     }
 
     @Override
-    public void onUpdateReceived(Update update) {
-
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    public void sendText(Long who, String what){
-        SendMessage sm = SendMessage.builder()
-                .chatId(who.toString())
-                .text(what).build();
-        try {
-            execute(sm);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+    public void consume(Update update) {
+        System.out.println(update);
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            handleTextMessage(update);
+        } else if (update.hasCallbackQuery()) {
+            handleCallbackQuery(update);
         }
     }
 
+    private void handleTextMessage(Update update) {
 
-    public void send(SendPhoto sm){ // метод обертка для отправки чтобы убрать try/catch
-        try {
-            execute(sm);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+        long chat_id = update.getMessage().getChatId();
+        String messageText = update.getMessage().getText();
+
+        switch (messageText) {
+            case "/start":
+                try {
+                    telegramClient.execute(MenuContent.createMainMenuMessage(chat_id));
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "/help":
+                try {
+                    telegramClient.execute(helpContent.createHelpMessage(chat_id));
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+                break;
         }
     }
-    public void send(SendMessage sm){ // метод обертка для отправки чтобы убрать try/catch
-        try {
-            execute(sm);
-        } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+
+    private void handleCallbackQuery(Update update) {
+        String call_data = update.getCallbackQuery().getData();
+        long message_id = update.getCallbackQuery().getMessage().getMessageId();
+        long chat_id = update.getCallbackQuery().getMessage().getChatId();
+
+        String answer;
+
+        if (call_data.equals("/search")) {
+            answer = "Поиск блюд, напитков. Вы нажали на первую кнопку.";
+            EditMessageText new_message = EditMessageText.builder()
+                    .chatId(chat_id)
+                    .messageId(toIntExact(message_id))
+                    .text(answer)
+                    .build();
+
+            try {
+                telegramClient.execute(new_message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (call_data.equals("/random")) {
+            answer = "Случайный рецепт. Вы нажали на вторую кнопку.";
+            EditMessageText new_message = EditMessageText.builder()
+                    .chatId(chat_id)
+                    .messageId(toIntExact(message_id))
+                    .text(answer)
+                    .build();
+
+            try {
+                telegramClient.execute(new_message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (call_data.equals("/wishlist")) {
+            answer = "Избранное";
+            EditMessageText new_message = EditMessageText.builder()
+                    .chatId(chat_id)
+                    .messageId(toIntExact(message_id))
+                    .text(answer)
+                    .build();
+
+            try {
+                telegramClient.execute(new_message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (call_data.equals("/catalog")) {
+            answer = "Каталог";
+            EditMessageText new_message = EditMessageText.builder()
+                    .chatId(chat_id)
+                    .messageId(toIntExact(message_id))
+                    .text(answer)
+                    .build();
+
+            try {
+                telegramClient.execute(new_message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (call_data.equals("/help")) {
+            try {
+                telegramClient.execute(helpContent.createHelpMessage(chat_id));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else if (call_data.equals("/back")) {
+            try {
+                telegramClient.execute(MenuContent.createMainMenuMessage(chat_id));
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        } else {
+            answer = "Неверный запрос";
+            EditMessageText new_message = EditMessageText.builder()
+                    .chatId(chat_id)
+                    .messageId(toIntExact(message_id))
+                    .text(answer)
+                    .build();
+
+            try {
+                telegramClient.execute(new_message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
         }
+
     }
 
-    @Override
-    public String getBotUsername() {
-        return "@Chef_A_P_bot";
-    }
-
-    @Override
-    public String getBotToken() {
-        return token.getToken();
-    }
 }
