844bbac6c459730a7c63bfc1b0a20498da28da39
novik <novikov.anton.serg@gmail.com>
2024-11-27 17:55:59 +0500

    Сделал наследование

--- src/main/java/Message/CatalogMessage.java
+++ src/main/java/Message/CatalogMessage.java
@@ -1,49 +1,20 @@
 package Message;
 
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CatalogMessage {
-    String CatalogText = "Вы попали в Каталог. Пока в разработке.";
-
-    public SendMessage createMessage(long chatId) {
-        return SendMessage.builder()
-                .chatId(chatId)
-                .text(CatalogText)
-                .replyMarkup(createKeyboard())
-                .build();
+public class CatalogMessage extends BaseMessage {
+    @Override
+    protected String getMessageText() {
+        return "Вы попали в Каталог. Пока в разработке.";
     }
 
-    public EditMessageText createEditMessage(long chatId, int messageId) {
-        return EditMessageText.builder()
-                .chatId(String.valueOf(chatId))
-                .messageId(messageId)
-                .text(CatalogText)
-                .replyMarkup(createKeyboard())
-                .build();
-    }
-
-    private InlineKeyboardMarkup createKeyboard() {
+    @Override
+    protected List<InlineKeyboardRow> createKeyboardRows() {
         List<InlineKeyboardRow> rows = new ArrayList<>();
-
         rows.add(createRow("На главное меню", "/back"));
-
-        return InlineKeyboardMarkup.builder().keyboard(rows).build();
-    }
-
-    private InlineKeyboardRow createRow(String buttonText, String callbackData) {
-        InlineKeyboardButton button = InlineKeyboardButton.builder()
-                .text(buttonText)
-                .callbackData(callbackData)
-                .build();
-        InlineKeyboardRow row = new InlineKeyboardRow();
-        row.add(button);
-        return row;
+        return rows;
     }
 }
--- src/main/java/Message/HelpMessage.java
+++ src/main/java/Message/HelpMessage.java
@@ -1,49 +1,22 @@
 package Message;
 
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageText;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardRow;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HelpMessage {
-    String HelpText = " Это бот для приготовление всяких блюд.\n" + "\n" + "Информация по всем функциям бота доступна по команде /start\n" + "\n" + "ℹ Подробная инструкция будет потом\n";
-
-    public SendMessage createMessage(long chatId) {
-        return SendMessage.builder()
-                .chatId(chatId)
-                .text(HelpText)
-                .replyMarkup(createKeyboard())
-                .build();
+public class HelpMessage extends BaseMessage {
+    @Override
+    protected String getMessageText() {
+        return "Это бот для приготовление всяких блюд.\n" +
+                "\nИнформация по всем функциям бота доступна по команде /start\n" +
+                "\nℹ Подробная инструкция будет потом\n";
     }
 
-    public EditMessageText createEditMessage(long chatId, int messageId) {
-        return EditMessageText.builder()
-                .chatId(String.valueOf(chatId))
-                .messageId(messageId)
-                .text(HelpText)
-                .replyMarkup(createKeyboard())
-                .build();
-    }
-
-    private InlineKeyboardMarkup createKeyboard() {
+    @Override
+    protected List<InlineKeyboardRow> createKeyboardRows() {
         List<InlineKeyboardRow> rows = new ArrayList<>();
-
         rows.add(createRow("На главное меню", "/back"));
-
-        return InlineKeyboardMarkup.builder().keyboard(rows).build();
-    }
-
-    private InlineKeyboardRow createRow(String buttonText, String callbackData) {
-        InlineKeyboardButton button = InlineKeyboardButton.builder()
-                .text(buttonText)
-                .callbackData(callbackData)
-                .build();
-        InlineKeyboardRow row = new InlineKeyboardRow();
-        row.add(button);
-        return row;
+        return rows;
     }
 }
