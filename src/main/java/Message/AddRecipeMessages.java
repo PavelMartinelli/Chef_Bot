51865befa8c70e3ba2b7373274e34a3de7fb1069
PavelMartinelli <pav.shipitsyn@gmail.com>
2024-12-01 16:00:44 +0500

    Добавление сообщений которые будут отправлятся при некоректных введенных данных пользователем

--- src/main/java/Message/AddRecipeMessages.java
+++ src/main/java/Message/AddRecipeMessages.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AddRecipeMessages {
+    //Текст сообщений
     private final String recipeNameMessage = "Хотите добавить свой рецепт? \n " +
             "Тогда введите название вашего рецепта на Русском языке";
 
@@ -20,6 +21,13 @@ public class AddRecipeMessages {
     private final String recipeImageMessage = "Отправте фото с вашим рецептом или URL ссылку на картинку";
 
     private final String recipeIngredientsMessage = "Напишите списком через запятую основные ингредиенты вашего рецепта";
+
+    //Текст сообщений при некоректных значениях
+    private final String incorrectNameMessage = "Некоректное имя рецепта";
+    private final String incorrectDescriptionMessage = "Некоректное описпние рецепта";
+    private final String incorrectImageMessage = "Некоректная картинка";
+    private final String incorrectIngredientsMessage = "Некоректный список ингредиентов";
+
 
     public EditMessageText addNameMessages(long chatId, int messageId){
         List<InlineKeyboardRow> rows = new ArrayList<>();
@@ -42,23 +50,51 @@ public class AddRecipeMessages {
                 .build();
     }
 
-    public SendMessage addDescriptionMessages(long chatId, int messageId){
+    public SendMessage addDescriptionMessages(long chatId){
         return SendMessage.builder()
                 .chatId(String.valueOf(chatId)).
                 text(recipeDescriptionMessage).
                 build();
     }
 
-    public SendMessage addRecipeImageMessage(long chatId, int messageId){
+    public SendMessage addRecipeImageMessage(long chatId){
         return SendMessage.builder()
                 .chatId(String.valueOf(chatId)).
                 text(recipeImageMessage).
                 build();
     }
-    public SendMessage addRecipeIngredientsMessage(long chatId, int messageId){
+    public SendMessage addRecipeIngredientsMessage(long chatId){
         return SendMessage.builder()
                 .chatId(String.valueOf(chatId)).
                 text(recipeIngredientsMessage).
+                build();
+    }
+
+    public SendMessage incorrectNameMessage(long chatId){
+        return SendMessage.builder()
+                .chatId(String.valueOf(chatId)).
+                text(incorrectNameMessage + recipeNameMessage).
+                build();
+    }
+
+    public SendMessage incorrectDescriptionMessage(long chatId){
+        return SendMessage.builder()
+                .chatId(String.valueOf(chatId)).
+                text(incorrectDescriptionMessage + recipeDescriptionMessage).
+                build();
+    }
+
+    public SendMessage incorrectImageMessage(long chatId){
+        return SendMessage.builder()
+                .chatId(String.valueOf(chatId)).
+                text(incorrectImageMessage + recipeImageMessage).
+                build();
+    }
+
+    public SendMessage incorrectIngredientsMessage(long chatId){
+        return SendMessage.builder()
+                .chatId(String.valueOf(chatId)).
+                text(incorrectIngredientsMessage + recipeIngredientsMessage).
                 build();
     }
 }
